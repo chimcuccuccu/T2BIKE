45ea667aa9033257feb6c2b2323e80bc2abf1970
Ngan cute hme <ngannguyenqpna@gmail.com>
2025-04-05 23:33:09 +0700

    session

--- backend/bikeshop/bikeshop/src/main/java/com/example/bikeshop/repository/UserRepository.java
+++ backend/bikeshop/bikeshop/src/main/java/com/example/bikeshop/repository/UserRepository.java
@@ -2,12 +2,16 @@ package com.example.bikeshop.repository;
 
 import com.example.bikeshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    Optional<User> findByUsername(@Param("username") String username);
+
     boolean existsByUsername(String username);
 }
