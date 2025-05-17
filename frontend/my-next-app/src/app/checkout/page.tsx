70e9869ce77b25dd3b63ab74f592ce7497a203d0
Ngan cute hme <ngannguyenqpna@gmail.com>
2025-05-17 11:04:39 +0700

    update

--- frontend/my-next-app/src/app/checkout/page.tsx
+++ frontend/my-next-app/src/app/checkout/page.tsx
@@ -4,9 +4,9 @@ import { HeaderPage } from "@/components/Header/header-page"
 export default function Home() {
   return (
     <div>
-        <main className="min-h-screen bg-gradient-to-b from-[#FFCDE2] to-[#FCFCF7] flex items-center justify-center p-4">
+      <main className="min-h-screen bg-gradient-to-b from-[#FFCDE2] to-[#FCFCF7] flex items-center justify-center p-4">
         <Checkout />
-        </main>
+      </main>
     </div>
   )
 }
